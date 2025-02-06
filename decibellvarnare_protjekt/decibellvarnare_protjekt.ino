c1739fe4dce5c7a22469c8fda065245596dde7cc
LukasWilhelmsson <Lukas.wilhelmsson@elev.ga.ntig.se>
2025-02-06 17:07:51 +0100

    Update decibellvarnare_protjekt.ino

--- decibellvarnare_protjekt/decibellvarnare_protjekt.ino
+++ decibellvarnare_protjekt/decibellvarnare_protjekt.ino
@@ -21,39 +21,39 @@ uint32_t yellow = strip.Color(255, 255, 0);
 uint32_t red = strip.Color(255, 0, 0);
 
 void setup() {
-// init communication
-Serial.begin(9600);
 
-// init hardware
-strip.begin();
-strip.show();
- // This fun
-for (int i = 0; i < NUM_PIXELS; i++) {
-  strip.setPixelColor(i, green);
+ // init communication
+ Serial.begin(9600);
+
+ // init hardware
+ strip.begin();
+ strip.show();
 }
-strip.show();
-}
-// This funktion reads the value from the KY-037 microphon and makes it to an int, then I got Serial.println to read from soundLevel to se the change in Serial Plotter. Then a for loop checks if i is bigger then the amount of light diods with NUM_PIXELS and afterwerds makes i+1 so that more and more lights shine. Then it checks if soundlevel is bigger then the inserted threeshwhold, if it is the ligt tern red if not it checks yellows threshwhold if it is then yellow light if not then the ligt terns green.
+
+/* This funktion reads the value from the KY-037 microphon and makes it to an int, then I got Serial.println to read from soundLevel to se the change in Serial Plotter.
+* Then a for loop checks if i is bigger then the amount of light diods with NUM_PIXELS and afterwerds makes i+1 so that more and more lights shine.
+* Then it checks if soundlevel is bigger then the inserted threeshwhold, if it is the ligt tern red if not it checks yellows threshwhold if it is then yellow light if not then the ligt terns green.
+*/
 void loop() {
+
  int soundLevel = analogRead(MIC_PIN);
 
  Serial.println(soundLevel);
-
- // 
+ 
  for (int i = 0; i < NUM_PIXELS; i++) {
-    if (soundLevel > 80) {
+    if (soundLevel > 90) {
   strip.setPixelColor(i, red);
-    } else if (soundLevel > 70) {
+    } else if (soundLevel > 75) {
   strip.setPixelColor(i, yellow);
     } else {
   strip.setPixelColor(i, green);
     } // går att ändra enkelt vid behov
  }
 
- 
+ // delay
  strip.show();
  delay(50);
-
+ // delay
  Serial.println(soundLevel);
  delay(100);
 }
